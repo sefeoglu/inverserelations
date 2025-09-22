15e2e3a2a4ba99141c8f4e3cc5674ec111cb08c4
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 <sefikaefeoglu@gmail.com>
2025-09-22 21:30:24 +0200

    add gemini

---
+++ src/llms/gemini.py
@@ -0,0 +1,49 @@
+import google.generativeai as genai
+import json
+import os
+import argparse
+def generate_text_with_gemini(question, config):
+    """
+    Gemini 2.5 Pro modelini kullanarak basit bir metin oluşturma örneği.
+    """
+    # API anahtarını ayarla
+    genai.configure(api_key=config["api_key"])
+    # Gemini 2.5 Pro modelini seçiyoruz
+    model = genai.GenerativeModel(config["model"])
+
+    response = model.generate_content(question)
+    return response.text
+
+
+def bulk_test(templates, out_file, config):
+    results = []
+    for item in templates:
+       
+       template_1 = item['template_1']
+       template_2 = item['template_2']
+
+       prediction_1 = generate_text_with_gemini(template_1, config)
+       prediction_2 = generate_text_with_gemini(template_2, config)
+
+       item['predictions_1'] = prediction_1.split('\n')
+       item['predictions_2'] = prediction_2.split('\n')
+
+       results.append(item)
+
+       with open(out_file, 'w') as f:
+                json.dump(results, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Combine contextual information from multiple sources.")
+    parser.add_argument("--input_file", type=str, default="/Users/sefika/phd_projects/converse_relations/data/experiment_2/templates_without_desc.json", help="Path to the input JSON file containing contextual information.")
+    parser.add_argument("--output_file", type=str, default="/Users/sefika/phd_projects/converse_relations/data/experiment_2/gemini_predictions_without_desc.json", help="Path to the output JSON file to save combined information.")
+    parser.add_argument("--config", type=str, default="/Users/sefika/phd_projects/converse_relations/data/gpt_key.json", help="Path to the config JSON file.")
+    
+    args = parser.parse_args()
+    input_data = json.load(open(args.input_file, 'r'))
+    out_path = args.output_file
+    config_path = args.config 
+    config = json.load(open(config_path, 'r'))
+    bulk_test(input_data, out_path, config)
+    print(f"Predictions saved to {out_path}")
