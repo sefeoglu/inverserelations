549dda16d68ef9e490f4a6a26dcaa88422375d6b
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 <sefikaefeoglu@gmail.com>
2025-11-04 15:20:08 +0100

    Revert "rename"
    This reverts commit 308626f2d792345e757dfec12ba7f732ee2eaeeb.

---
+++ src/question_generation/template_2.py
@@ -0,0 +1,198 @@
+
+from random import shuffle
+import os
+import sys
+import json
+
+def read_json_file(file_path):
+    """
+    Reads a JSON file and returns its content.
+    
+    Args:
+        file_path (str): The path to the JSON file.
+        
+    Returns:
+        dict: The content of the JSON file.
+    """
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"The file {file_path} does not exist.")
+    
+    with open(file_path, 'r') as file:
+        return json.load(file)
+    
+def write_json_file(data, file_path):
+    """
+    Writes data to a JSON file.
+    
+    Args:
+        data (dict): The data to be written to the file.
+        file_path (str): The path to the JSON file.
+    """
+    if os.path.exists(file_path):
+        print(f"Warning: The file {file_path} already exists and will be overwritten.", file=sys.stderr)
+    
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def write_json_str(data, file_path):
+    """
+    Writes data to a JSON file, converting non-serializable types (like sets).
+
+    Args:
+        data (dict): The data to be written to the file.
+        file_path (str): The path to the JSON file.
+    """
+    def convert(obj):
+        if isinstance(obj, set):
+            return list(obj)
+        raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
+
+    if os.path.exists(file_path):
+        print(f"Warning: The file {file_path} already exists and will be overwritten.", file=sys.stderr)
+    
+    json_str = json.dumps(data, indent=4, default=convert)
+    
+    with open(file_path, 'w') as file:
+        file.write(json_str)
+        
+def write_turtle_to_ttl(file_path, content):
+    """
+    Writes Turtle content to a TTL file.
+    
+    Args:
+        file_path (str): The path to the TTL file.
+        content (str): The Turtle content to be written.
+    """
+    content = "\n".join(content)  # Ensure content is a single string
+    content = content.replace(' .', '.')
+    content = content.replace(' ;', ';')
+
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+def relation_info(item, relations):
+    rel_desc1 = ""
+    rel_desc2 = ""
+    rel_name1 = ""
+    rel_name2 = ""
+    for relation in relations:
+        if relation['pid'] == item['possible_probs'][0]:
+            rel_desc1 = relation['definition']
+            rel_name1 = relation['name']
+        if relation['pid'] == item['possible_probs'][1]:
+            rel_desc2 = relation['definition']
+            rel_name2 = relation['name']
+    return rel_name1, rel_name2, rel_desc1, rel_desc2
+        
+def get_template_first(item: str, relations) -> str:
+    """
+    _summary_
+    
+    Args:
+        item (str): _description_
+        Returns:
+            str: _description_"""
+    rel_name1, rel_name2, rel_desc1, rel_desc2 = relation_info(item, relations)
+
+    template = f""" What is the relation from [HEAD]{item['h'][0]} [/HEAD] to [TAIL]{item['t'][0]} [/TAIL] in the sentence?
+                Sentence: {' '.join(item['tokens'])}
+                A .) {rel_name1}: {rel_desc1}.
+                B .) {rel_name2}: {rel_desc2}.
+                C .) None of the above.
+                Please choose A, B, or C.
+                Answer:"""
+    return template, rel_name1
+def get_template_second(item: str, relations) -> str:
+    """
+    _summary_
+    
+    Args:
+        item (str): _description_
+        Returns:
+            str: _description_"""
+    
+    rel_name1, rel_name2, rel_desc1, rel_desc2 = relation_info(item, relations)
+
+    template = f""" What is the relation from [TAIL] {item['t'][0]} [/TAIL] to [HEAD] {item['h'][0]} [/HEAD] in the sentence?
+                Sentence: {' '.join(item['tokens'])}
+                A .) {rel_name1}: {rel_desc1}.
+                B .) {rel_name2}: {rel_desc2}.
+                C .) None of the above.
+                Please choose A, B, or C.
+                Answer:"""
+    return template, rel_name2
+def get_template_nodesc_first(item: str, relations) -> str:
+    """
+    _summary_
+    
+    Args:
+        item (str): _description_
+        Returns:
+            str: _description_"""
+    rel_name1, rel_name2, _, _ = relation_info(item, relations)
+
+    template = f""" What is the relation from [HEAD] {item['h'][0]} [/HEAD] to [TAIL] {item['t'][0]} [/TAIL] in the sentence?
+                Sentence: {' '.join(item['tokens'])}
+                A .) {rel_name1}.
+                B .) {rel_name2}.
+                C .) None of the above.
+                Please choose A, B, or C.
+                Answer:"""
+    return template, rel_name1
+def get_template_nodesc_second(item: str, relations) -> str:
+    """
+    _summary_
+    
+    Args:
+        item (str): _description_
+        Returns:
+            str: _description_"""
+    
+    rel_name1, rel_name2, _, _ = relation_info(item, relations)
+
+    template = f""" What is the relation from [TAIL] {item['t'][0]} [/TAIL] to [HEAD] {item['h'][0]} [/HEAD] in the sentence?
+                Sentence: {' '.join(item['tokens'])}
+                A .) {rel_name1}.
+                B .) {rel_name2}.
+                C .) None of the above.
+                Please choose A, B, or C.
+                Answer:"""
+    
+    return template, rel_name2
+
+def all_data(data, relations, out_file):
+    templates = []
+
+    for item in data:
+        template, ground_truth_1 = get_template_first(item, relations)
+        item['template_1'], item['ground_truth_1'] = template, ground_truth_1
+
+        template, ground_truth_2 = get_template_second(item, relations)
+        item['template_2'], item['ground_truth_2'] = template, ground_truth_2
+        templates.append(item)
+
+    shuffle(templates)
+    write_json_file(templates, out_file)
+
+
+def all_data_nodesc(data, relations, out_file):
+    templates = []
+
+    for item in data:
+        template, ground_truth_1 = get_template_nodesc_first(item, relations)
+        item['template_1'], item['ground_truth_1'] = template, ground_truth_1
+
+        template, ground_truth_2 = get_template_nodesc_second(item, relations)
+        item['template_2'], item['ground_truth_2'] = template, ground_truth_2
+        templates.append(item)
+    shuffle(templates)
+    write_json_file(templates, out_file)
+
+if __name__ == "__main__":
+    data = read_json_file("/Users/sefika/phd_projects/converse_relations/data/cleaned_asymetrics.json")
+    relations = read_json_file("/Users/sefika/phd_projects/converse_relations/data/subset_inverse_relations.json")
+    out_file = "/Users/sefika/phd_projects/converse_relations/data/experiment_2/templates_with_desc.json"
+    all_data(data, relations, out_file)
+    all_data_nodesc(data, relations, out_file.replace(".json", "_with_desc.json"))
